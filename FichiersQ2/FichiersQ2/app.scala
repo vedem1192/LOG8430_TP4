c8891f20b15492590b6b5907588bd1dfcfc64be9
Laurent Caron <laurentcarondufour@gmail.com>
2018-12-03 23:58:54 -0500

    Modified app.scala

--- FichiersQ2/FichiersQ2/app.scala
+++ FichiersQ2/FichiersQ2/app.scala
@@ -1,22 +1,34 @@
 import org.apache.spark.mllib.fpm.FPGrowth
 import org.apache.spark.rdd.RDD
-import com.datastax.spark.connector._, org.apache.spark.SparkContext, org.apache.spark.SparkContext._, org.apache.spark.SparkConf, org.apache.spark.sql.cassandra._
+import com.datastax.spark.connector._
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.cassandra._
 
-val conf = new SparkConf(true).set("spark.cassandra.connection.host", "127.0.0.1")
-val rdd = sc.cassandraTable("dev", "facture")
+object Main {
+        def main(args: Array[String]) {
+                //val conf = new SparkConf(true).set("spark.cassandra.connection.host", "127.0.0.1")
+                val conf = new SparkConf().setAppName("TP4").setMaster("local[2]").set("spark.cassandra.connection.host$                val sc = new SparkContext(conf)
+                val rdd = sc.cassandraTable("dev", "facture").select("name")
+                rdd.collect().foreach(println)
+                val data = sc.textFile("data.txt")
 
+                val transactions: RDD[Array[String]] = data.map(s => s.trim.split(' '))
+                val fpg = new FPGrowth()
+                        .setMinSupport(0.2)
+                        .setNumPartitions(10)
+                val model = fpg.run(transactions)
 
-val fpg = new FPGrowth()
-  .setMinSupport(0.2)
-  .setNumPartitions(10)
-val model = fpg.run(rdd)
+                model.freqItemsets.collect().foreach { itemset =>
+                        println(s"${itemset.items.mkString("[", ",", "]")},${itemset.freq}")
+                }
 
-model.freqItemsets.collect().foreach { itemset =>
-  println(s"${itemset.items.mkString("[", ",", "]")},${itemset.freq}")
-}
-
-val minConfidence = 0.8
-model.generateAssociationRules(minConfidence).collect().foreach { rule =>
-  println(s"${rule.antecedent.mkString("[", ",", "]")}=> " +
-    s"${rule.consequent .mkString("[", ",", "]")},${rule.confidence}")
+                val minConfidence = 0.8
+                model.generateAssociationRules(minConfidence).collect().foreach { rule =>
+                        println(s"${rule.antecedent.mkString("[", ",", "]")}=> " +
+                        s"${rule.consequent .mkString("[", ",", "]")},${rule.confidence}")
+                }
+                sc.stop
+        }
 }
